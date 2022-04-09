921fd86a95aa36b2dcc3a3825dbf0ea1d0bb5aad
Istip <isticsek@gmail.com>
2022-04-09 15:21:56 +0300

    fonts added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import styled from 'styled-components';
+
+// Fonts used
+import './fonts/Uncut-Sans-Regular.otf';
+import './fonts/Uncut-Sans-Medium.otf';
+import './fonts/Uncut-Sans-Semibold.otf';
+import './fonts/Uncut-Sans-Bold.otf';
+
 function App() {
   return (
     <div>
-      <h1>Portfolio</h1>
+      <Title>Portfolio</Title>
+      <Description>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque nulla,
+        doloribus eos in voluptate ipsam non, deserunt exercitationem explicabo
+        culpa, molestias atque provident et accusamus. Vel assumenda cupiditate
+        inventore eos!
+      </Description>
     </div>
   );
 }
+
+const Title = styled.h1`
+  font-family: 'Bold';
+  font-size: 72px;
+`;
+
+const Description = styled.div`
+  font-family: 'Regular';
+  font-size: 20px;
+`;
 
 export default App;
