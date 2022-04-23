9b455a72ebb2a0f99796d4e0897f16757e0a596a
Istip <isticsek@gmail.com>
2022-04-23 16:38:24 +0300

    removed fonts

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import styled from 'styled-components';
-
-// Fonts used
-import './fonts/Uncut-Sans-Regular.otf';
-import './fonts/Uncut-Sans-Medium.otf';
-import './fonts/Uncut-Sans-Semibold.otf';
-import './fonts/Uncut-Sans-Bold.otf';
+// import styled from 'styled-components';
 
 function App() {
   return (
     <div>
-      <Title>Portfolio</Title>
-      <Description>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque nulla,
-        doloribus eos in voluptate ipsam non, deserunt exercitationem explicabo
-        culpa, molestias atque provident et accusamus. Vel assumenda cupiditate
-        inventore eos!
-      </Description>
+      <h1>Portfolio</h1>
     </div>
   );
 }
-
-const Title = styled.h1`
-  font-family: 'Bold';
-  font-size: 72px;
-`;
-
-const Description = styled.div`
-  font-family: 'Regular';
-  font-size: 20px;
-`;
 
 export default App;
