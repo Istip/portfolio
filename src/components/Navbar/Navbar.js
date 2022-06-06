9c65db514d4b5195a903344325d18543a8dce556
Istip <isticsek@gmail.com>
2022-06-06 18:17:58 +0300

    Picked out json data to separetad file

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,42 +4,9 @@ import {
   NavbarContent as Content,
   NavbarWrapper as Wrapper,
 } from './Navbar.styles';
+import { icons, buttons } from './navbarData';
 
 const Navbar = ({ background, ...props }) => {
-  // Array of icons to render
-  const icons = [
-    { name: 'facebook', route: 'https://facebook.com/pasztor.isti/' },
-    { name: 'instagram', route: 'https://instagram.com/pasztor.isti/' },
-    { name: 'github', route: 'https://github.com/Istip/' },
-  ];
-
-  // Array of buttons to render
-  const buttons = [
-    {
-      text: 'about',
-      margin: '0 10px',
-      onClick: () => {
-        document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
-      },
-    },
-    {
-      text: 'work',
-      margin: '0 10px 0 0',
-      onClick: () => {
-        document.getElementById('work').scrollIntoView({ behavior: 'smooth' });
-      },
-    },
-    {
-      text: 'contact',
-      variant: 'secondary',
-      onClick: () => {
-        document
-          .getElementById('contact')
-          .scrollIntoView({ behavior: 'smooth' });
-      },
-    },
-  ];
-
   const headerColor =
     background === tokens.colors.primary700
       ? tokens.colors.white
