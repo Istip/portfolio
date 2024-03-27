4fabd04449e2325ba68baa4696ab05bb60a08b36
Pasztor Isti <isticsek@gmail.com>
2024-03-27 20:32:54 +0200

    create temporary logo

---
+++ src/components/Navbar/Logo.tsx
@@ -0,0 +1,10 @@
+import Text from "../Text/Text";
+
+const Logo = () => (
+  <div className="center gap-2">
+    <div className="w-4 aspect-square rounded-full bg-primary" />
+    <Text type="expanded">isti</Text>
+  </div>
+);
+
+export default Logo;
