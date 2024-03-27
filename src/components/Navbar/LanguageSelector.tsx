b2988bda1b297c4d54dfb1f2ac70a25285ff02a2
Pasztor Isti <isticsek@gmail.com>
2024-03-27 20:22:59 +0200

    update nav components

--- src/components/Navbar/LanguageSelector.tsx
+++ src/components/Navbar/LanguageSelector.tsx
@@ -1,20 +1,24 @@
 "use client";
 
 import { useChangeLocale, useCurrentLocale } from "@/locales/client";
+import Text from "../Text/Text";
 
 const LanguageSelector = () => {
   const changeLocale = useChangeLocale();
   const locale = useCurrentLocale();
 
   const toggleLocale = () => changeLocale(locale === "en" ? "hu" : "en");
-  const otherLocale = locale === "en" ? "hu" : "en";
+  const swapLocale = locale === "en" ? "hu" : "en";
 
   return (
-    <div>
-      <button onClick={toggleLocale} className="uppercase">
-        {otherLocale}
+    <Text
+      as="div"
+      className="text-xs bg-white p-2 rounded-full border border-stone-900"
+    >
+      <button onClick={toggleLocale} className="uppercase bg-white center">
+        {swapLocale}
       </button>
-    </div>
+    </Text>
   );
 };
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import LanguageSelector from "./LanguageSelector";
+import Icon from "../Icon/Icon";
 
 const Navbar = () => {
   return (
     <div className="w-full bg-stone-100 p-5">
       <ul className="flex items-center justify-between mx-auto">
         <li>LOGO</li>
-        <ul className="center gap-2">
+        <ul className="center gap-4">
           <li>
             <LanguageSelector />
           </li>
-          <li>MENU</li>
+          <li className="center gap-2">
+            <div>MENU</div>
+            <div>
+              <Icon name="menu" />
+            </div>
+          </li>
         </ul>
       </ul>
     </div>
