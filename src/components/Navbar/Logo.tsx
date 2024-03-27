c0f460f97d4b3b2abd75c97960ca290b53ec1c2e
Pasztor Isti <isticsek@gmail.com>
2024-03-27 21:42:53 +0200

    update logo to use statuses

--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -1,15 +1,33 @@
+import { getI18n, getScopedI18n } from "@/locales/server";
 import Text from "../Text/Text";
 
-const Logo = () => (
-  <div className="center gap-2 text-dark">
-    <div className="relative">
-      <div className="w-4 aspect-square rounded-full bg-primary animate-ping" />
-      <div className="absolute inset-0 flex items-center justify-center">
-        <div className="w-3 aspect-square rounded-full bg-primary" />
-      </div>
-    </div>
-    <Text type="expanded">isti</Text>
-  </div>
-);
+const statuses = [
+  { name: "available", color: "bg-primary" },
+  { name: "partiallyAvailable", color: "bg-yellow-500" },
+  { name: "veryBusy", color: "bg-red-500" },
+];
 
-export default Logo;
+export default async function Logo() {
+  const t = await getScopedI18n("status");
+
+  let { name, color } = statuses[0];
+
+  let key: "available" | "partiallyAvailable" | "veryBusy" = name as
+    | "available"
+    | "partiallyAvailable"
+    | "veryBusy";
+
+  return (
+    <div className="center gap-2 text-dark">
+      <div className="relative">
+        <div
+          className={`${color} w-4 aspect-square rounded-full animate-ping`}
+        />
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className={`${color} w-3 aspect-square rounded-full`} />
+        </div>
+      </div>
+      <Text type="expanded">{t(key)}</Text>
+    </div>
+  );
+}
