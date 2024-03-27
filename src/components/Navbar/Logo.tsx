4088149bf6b5742948d82eb7e9d16dd400198aba
Pasztor Isti <isticsek@gmail.com>
2024-03-27 21:20:25 +0200

    fix logo to show my current status

--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -1,8 +1,13 @@
 import Text from "../Text/Text";
 
 const Logo = () => (
-  <div className="center gap-2">
-    <div className="w-4 aspect-square rounded-full bg-primary" />
+  <div className="center gap-2 text-dark">
+    <div className="relative">
+      <div className="w-4 aspect-square rounded-full bg-primary animate-ping" />
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="w-3 aspect-square rounded-full bg-primary" />
+      </div>
+    </div>
     <Text type="expanded">isti</Text>
   </div>
 );
