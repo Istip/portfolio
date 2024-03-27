4cf7b7b88467c990b317d444af51da2bd534210d
Pasztor Isti <isticsek@gmail.com>
2024-03-27 22:17:54 +0200

    update main page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,16 +1,9 @@
-"use client";
-
-import Text from "@/components/Text/Text";
-import { useI18n } from "@/locales/client";
+import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
-  const t = useI18n();
-
   return (
-    <div>
-      <Text as="h1" type="mona" className="text-dark text-7xl">
-        {t("hello")} Istvan Pasztor
-      </Text>
+    <div className="md:px-[50px] px-5">
+      <Hero />
     </div>
   );
 }
