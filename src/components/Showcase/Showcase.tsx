dcdbd9a22051fd7ba2070bc62cd39d6b8c5c2255
Pasztor Isti <isticsek@gmail.com>
2024-03-27 21:19:28 +0200

    create showcase placeholder component

---
+++ src/components/Showcase/Showcase.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Text from "../Text/Text";
+
+const Showcase = () => {
+  return (
+    <aside className="sticky w-full h-screen bg-dark">
+      <Text
+        as="h1"
+        type="expanded"
+        className="text-white center h-full text-5xl"
+      >
+        SHOWCASE
+      </Text>
+    </aside>
+  );
+};
+
+export default Showcase;
