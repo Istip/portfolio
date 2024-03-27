b469ae867fb802ed948015f52b799a1486f3b3ce
Pasztor Isti <isticsek@gmail.com>
2024-03-27 22:17:42 +0200

    make the showcase sticky

--- src/components/Showcase/Showcase.tsx
+++ src/components/Showcase/Showcase.tsx
@@ -3,7 +3,7 @@ import Text from "../Text/Text";
 
 const Showcase = () => {
   return (
-    <aside className="sticky w-full h-screen bg-dark">
+    <aside className="sticky top-0 bottom-0 w-full h-screen bg-dark">
       <Text
         as="h1"
         type="expanded"
