64c56a1c4031ed04e02ae6448eaecd9c92c6628f
Pasztor Isti <isticsek@gmail.com>
2024-03-28 19:29:09 +0200

    add more test sections

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import Hero from "@/components/Hero/Hero";
 import Section from "@/components/Sections/Section";
-import Text from "@/components/Text/Text";
+import About from "@/components/Sections/About";
 
 export default function Home() {
   return (
@@ -9,23 +9,13 @@ export default function Home() {
       <div className="md:px-[50px] px-5 py-9">
         <Hero />
       </div>
-      <Section
-        number="01"
-        title="About me"
-        colors={["text-primaryDark", "text-dark"]}
-        bg="bg-primary"
-      >
-        {Array.from({ length: 10 }).map((_, i) => (
-          <Fragment key={i}>
-            <Text as="h1" className="text-2xl">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima
-              non ab consequuntur, reiciendis saepe magni hic asperiores dicta
-              perspiciatis placeat voluptatum quisquam doloremque delectus
-              repudiandae, pariatur dolorem ducimus tempora distinctio!
-            </Text>
-            <br />
-          </Fragment>
-        ))}
+
+      <Section number={1} title="about">
+        <About />
+      </Section>
+
+      <Section number={2} title="services" from="#3EEA8D" to="#111111">
+        <About />
       </Section>
     </>
   );
