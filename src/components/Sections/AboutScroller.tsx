f3ea83e02e17403b6f88285a84c219a8766e9e81
Pasztor Isti <isticsek@gmail.com>
2024-03-29 10:40:01 +0200

    create horizontal scroller component for the future about me section

---
+++ src/components/Sections/AboutScroller.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+const AboutScroller = () => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({ target: targetRef });
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-80%"]);
+
+  return (
+    <section
+      ref={targetRef}
+      className="relative z-10 h-[700vh] rounded-2xl mt-40 mx-2 md:mx-5 my-10"
+    >
+      <div className="sticky top-0 flex h-screen items-center overflow-hidden">
+        <motion.div className="flex gap-40 mb-20" style={{ x }}>
+          {Array.from({ length: 5 }).map((_, i) => (
+            <div
+              key={i}
+              className="w-[500px] h-64 bg-primaryDark rounded-2xl flex"
+            />
+          ))}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutScroller;
