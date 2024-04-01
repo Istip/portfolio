5feb1e908e6bf8db2ebc8acd1f15e56dd99b90b1
Pasztor Isti <isticsek@gmail.com>
2024-04-01 19:50:16 +0300

    create client side contact form

---
+++ src/components/Sections/Contact/ContactForm.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import DoubleButton from "@/components/Button/DoubleButton";
+import Text from "@/components/Text/Text";
+import { useState } from "react";
+
+export default function ContactForm() {
+  const initialFormData = { name: "", email: "", subject: "", message: "" };
+  const [formData, setFormData] = useState(initialFormData);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault();
+      setLoading(true);
+      console.log(formData);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+      setFormData(initialFormData);
+    }
+  };
+
+  const labelStyles = "text-primaryDark font-bold cursor-pointer uppercase";
+  const inputStyles =
+    "rounded-2xl border border-dark bg-white text-dark p-[10px] disabled:opacity-50 w-full focus:outline-none focus:ring-2 focus:ring-primaryDark focus:border-transparent transition-colors duration-300";
+
+  return (
+    <div className="mt-6">
+      <form className="space-y-5" onSubmit={handleSubmit}>
+        <div>
+          <Text className="mb-[5px]">
+            <label htmlFor="name" className={labelStyles}>
+              Name
+            </label>
+          </Text>
+          <input
+            required
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            placeholder="Name"
+            className={inputStyles}
+            onChange={handleChange}
+            disabled={loading}
+          />
+        </div>
+
+        <div>
+          <Text className="mb-[5px]">
+            <label htmlFor="email" className={labelStyles}>
+              Email
+            </label>
+          </Text>
+          <input
+            required
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            placeholder="Email"
+            className={inputStyles}
+            onChange={handleChange}
+            disabled={loading}
+          />
+        </div>
+
+        <div>
+          <Text className="mb-[5px]">
+            <label htmlFor="subject" className={labelStyles}>
+              Subject
+            </label>
+          </Text>
+          <input
+            required
+            type="text"
+            id="subject"
+            name="subject"
+            value={formData.subject}
+            placeholder="Subject"
+            className={inputStyles}
+            onChange={handleChange}
+            disabled={loading}
+          />
+        </div>
+
+        <div>
+          <Text className="mb-[5px]">
+            <label htmlFor="message" className={labelStyles}>
+              Message
+            </label>
+          </Text>
+          <textarea
+            required
+            id="message"
+            name="message"
+            placeholder="Message"
+            value={formData.message}
+            className={inputStyles}
+            rows={6}
+            onChange={handleChange}
+            disabled={loading}
+          />
+        </div>
+
+        <div className="w-full">
+          <DoubleButton
+            type="submit"
+            className="border border-primary bg-primary"
+            withIcon
+          >
+            {loading ? "Sending..." : "Submit"}
+          </DoubleButton>
+        </div>
+      </form>
+    </div>
+  );
+}
