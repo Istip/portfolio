87734fd88a0fab966d720e6fe371bb36ae8f2c28
Pasztor Isti <isticsek@gmail.com>
2024-04-01 20:46:56 +0300

    add marquue and test out on showcase

--- src/components/Showcase/Showcase.tsx
+++ src/components/Showcase/Showcase.tsx
@@ -1,16 +1,47 @@
 import React from "react";
 import Text from "../Text/Text";
+import Marquee from "react-fast-marquee";
 
 const Showcase = () => {
+  // random number between 400 and 800
+  const randomNumber = () => Math.floor(Math.random() * (800 - 400) + 400);
+  const randomWidth = `w-[${randomNumber()}px]`;
+
+  console.log("✅  randomWidth:", randomWidth);
+
   return (
-    <aside className="sticky top-0 bottom-0 w-full h-screen bg-dark">
-      <Text
-        as="h1"
-        type="expanded"
-        className="text-white center h-full text-5xl"
+    <aside className="sticky top-0 bottom-0 w-full h-screen bg-dark grid grid-cols-1 py-[70px] px-10 gap-10 select-none">
+      <Marquee
+        autoFill
+        direction="left"
+        speed={20}
+        loop={0}
+        className="w-full overflow-y-hidden"
       >
-        SHOWCASE
-      </Text>
+        <Text
+          as="div"
+          type="expanded"
+          className="text-white center text-5xl mr-5 bg-primaryDark/20 p-10 h-screen"
+        >
+          IMAGE
+        </Text>
+      </Marquee>
+
+      <Marquee
+        autoFill
+        direction="right"
+        speed={20}
+        loop={0}
+        className="w-full overflow-y-hidden"
+      >
+        <Text
+          as="div"
+          type="expanded"
+          className="text-white center text-5xl mr-5 bg-primaryDark/20 p-10 h-screen"
+        >
+          IMAGE
+        </Text>
+      </Marquee>
     </aside>
   );
 };
