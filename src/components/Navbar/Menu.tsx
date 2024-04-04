bc0c03a1a7e055c2925972c5b80423d47f7b3d00
Pasztor Isti <isticsek@gmail.com>
2024-04-05 00:33:33 +0300

    create full menu boilerplate

---
+++ src/components/Navbar/Menu.tsx
@@ -0,0 +1,52 @@
+import { useEffect } from "react";
+import { motion } from "framer-motion";
+import Icon from "../Icon/Icon";
+
+interface Props {
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function Menu({ setOpen }: Props) {
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [setOpen]);
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: "100vw" }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 1, x: "100vw" }}
+      transition={{ duration: 0.25, ease: "easeInOut" }}
+      className="fixed top-0 left-0 right-0 w-full h-full bg-dark z-[99999] center"
+    >
+      <button
+        className="absolute right-6 top-6 pl-6"
+        onClick={() => setOpen(false)}
+      >
+        <Icon name="close" />
+      </button>
+      <div className="grid grid-cols-3 w-full h-full">
+        <motion.div
+          className="bg-primary center"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+        >
+          1
+        </motion.div>
+        <div className="col-span-2 center">2</div>
+      </div>
+    </motion.div>
+  );
+}
--- src/components/Navbar/MenuIcon.tsx
+++ src/components/Navbar/MenuIcon.tsx
@@ -1,19 +1,33 @@
-import { getI18n } from "@/locales/server";
-import Text from "../Text/Text";
+"use client";
 
-export default async function MenuIcon() {
-  const t = await getI18n();
+import { useState } from "react";
+import { useI18n } from "@/locales/client";
+import Text from "../Text/Text";
+import Menu from "./Menu";
+import { AnimatePresence } from "framer-motion";
+
+export default function MenuIcon() {
+  const [open, setOpen] = useState(false);
+  const t = useI18n();
 
   return (
-    <li>
-      <button className="center gap-2 group uppercase">
-        <Text>{t("menu")}</Text>
-        <div className="transition-all">
-          <div className="h-[1px] w-8 transition-all duration-200 group-hover:w-16 bg-light rounded-full mb-1" />
-          <div className="h-[1px] w-8 transition-all duration-200 group-hover:w-16 bg-light rounded-full mb-1" />
-          <div className="h-[1px] w-8 transition-all duration-200 group-hover:w-16 bg-light rounded-full" />
-        </div>
-      </button>
-    </li>
+    <>
+      <AnimatePresence mode="wait">
+        {open && <Menu setOpen={setOpen} />}
+      </AnimatePresence>
+      <li>
+        <button
+          className="center gap-2 group uppercase"
+          onClick={() => setOpen(true)}
+        >
+          <Text>{t("menu")}</Text>
+          <div className="transition-all">
+            <div className="h-[1px] w-8 transition-all duration-200 group-hover:w-16 bg-light rounded-full mb-1" />
+            <div className="h-[1px] w-8 transition-all duration-200 group-hover:w-16 bg-light rounded-full mb-1" />
+            <div className="h-[1px] w-8 transition-all duration-200 group-hover:w-16 bg-light rounded-full" />
+          </div>
+        </button>
+      </li>
+    </>
   );
 }
