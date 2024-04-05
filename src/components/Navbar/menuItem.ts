15ca852bf99a39502439a010e6ba796131a0fc9a
Pasztor Isti <isticsek@gmail.com>
2024-04-05 22:34:26 +0300

    create menu links

---
+++ src/components/Navbar/menuItem.ts
@@ -0,0 +1,7 @@
+export const menuItems = [
+  { name: "Home", link: "/" },
+  { name: "About", link: "/about" },
+  { name: "Showcase", link: "/showcase" },
+  { name: "Services", link: "/services" },
+  { name: "Contact", link: "/contact" },
+];
