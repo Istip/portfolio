1555a8ca4b11ebf5138503c10b6028932e052d7c
Pasztor Isti <isticsek@gmail.com>
2024-04-05 23:53:30 +0300

    lowercase menuItems

--- src/components/Navbar/menuItem.ts
+++ src/components/Navbar/menuItem.ts
@@ -6,9 +6,9 @@ export type MenuItem = {
 };
 
 export const menuItems = [
-  { id: 1, bg: "bg-red-500", name: "Home", link: "/" },
-  { id: 2, bg: "bg-sky-500", name: "About", link: "/about" },
-  { id: 3, bg: "bg-amber-500", name: "Showcase", link: "/showcase" },
-  { id: 4, bg: "bg-lime-500", name: "Services", link: "/services" },
-  { id: 5, bg: "bg-orange-500", name: "Contact", link: "/contact" },
+  { id: 1, bg: "bg-red-500", name: "home", link: "/" },
+  { id: 2, bg: "bg-sky-500", name: "about", link: "/about" },
+  { id: 3, bg: "bg-amber-500", name: "showcase", link: "/showcase" },
+  { id: 4, bg: "bg-lime-500", name: "services", link: "/services" },
+  { id: 5, bg: "bg-orange-500", name: "contact", link: "/contact" },
 ];
