872485b863a83ae6e0b3a172be0efb3f05e3d5a4
Pasztor Isti <isticsek@gmail.com>
2024-04-05 23:25:08 +0300

    create basic menu stuff

--- src/components/Navbar/Menu.tsx
+++ src/components/Navbar/Menu.tsx
@@ -1,12 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import Icon from "../Icon/Icon";
+import { MenuItem, menuItems } from "./menuItem";
+import Link from "next/link";
+import Text from "../Text/Text";
 
 interface Props {
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export default function Menu({ setOpen }: Props) {
+  const [selected, setSelected] = useState<null | MenuItem>(null);
+
   useEffect(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
       if (event.key === "Escape") {
@@ -21,6 +26,10 @@ export default function Menu({ setOpen }: Props) {
     };
   }, [setOpen]);
 
+  useEffect(() => {
+    console.log("✅ selected:", selected);
+  }, [selected]);
+
   return (
     <motion.div
       initial={{ opacity: 0, x: "100vw" }}
@@ -28,6 +37,7 @@ export default function Menu({ setOpen }: Props) {
       exit={{ opacity: 1, x: "100vw" }}
       transition={{ duration: 0.25, ease: "easeInOut" }}
       className="fixed top-0 left-0 right-0 w-full h-full bg-dark z-[99999] center"
+      onClick={() => setOpen(false)}
     >
       <button
         className="absolute right-6 top-6 pl-6"
@@ -37,15 +47,32 @@ export default function Menu({ setOpen }: Props) {
       </button>
       <div className="grid grid-cols-3 w-full h-full">
         <motion.div
-          className="bg-primary center"
+          className={`center transition-colors ease-in-out duration-500 ${
+            selected?.bg ?? "bg-primary"
+          }`}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 1, delay: 0.5 }}
         >
-          1
+          {selected?.name}
         </motion.div>
-        <div className="col-span-2 center">2</div>
+        <div className="col-span-2 flex items-center p-10 group">
+          <ul className="flex flex-col">
+            {menuItems.map((item) => (
+              <Link
+                href={item.link}
+                key={item.id}
+                onMouseEnter={() => setSelected(item)}
+                onMouseLeave={() => setSelected(null)}
+              >
+                <Text as="li" className="uppercase text-7xl">
+                  {item.name}
+                </Text>
+              </Link>
+            ))}
+          </ul>
+        </div>
       </div>
     </motion.div>
   );
--- src/components/Navbar/menuItem.ts
+++ src/components/Navbar/menuItem.ts
@@ -1,7 +1,14 @@
+export type MenuItem = {
+  id: number;
+  bg: string;
+  name: string;
+  link: string;
+};
+
 export const menuItems = [
-  { name: "Home", link: "/" },
-  { name: "About", link: "/about" },
-  { name: "Showcase", link: "/showcase" },
-  { name: "Services", link: "/services" },
-  { name: "Contact", link: "/contact" },
+  { id: 1, bg: "bg-red-500", name: "Home", link: "/" },
+  { id: 2, bg: "bg-sky-500", name: "About", link: "/about" },
+  { id: 3, bg: "bg-amber-500", name: "Showcase", link: "/showcase" },
+  { id: 4, bg: "bg-lime-500", name: "Services", link: "/services" },
+  { id: 5, bg: "bg-orange-500", name: "Contact", link: "/contact" },
 ];
