06f815f238c1d8a941c0adbb7ef815f90f56d3ca
Pasztor Isti <isticsek@gmail.com>
2024-04-07 02:00:08 +0300

    fix top padding for right side of navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="w-full fixed z-10 p-5 text-light">
       <ul className="flex items-center justify-between mx-auto">
         <Status />
-        <ul className="center gap-4 px-2 py-2 bg-dark rounded-bl-2xl">
+        <ul className="center gap-4 px-2 pb-2 bg-dark rounded-bl-2xl">
           <LanguageSelector />
           <MenuIcon />
         </ul>
