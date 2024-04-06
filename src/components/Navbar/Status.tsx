cc72e36fbe02acf0893c73edbfd15a73086713ba
Pasztor Isti <isticsek@gmail.com>
2024-04-07 01:34:40 +0300

    add status functionality to bring back to homepage

--- src/components/Navbar/Status.tsx
+++ src/components/Navbar/Status.tsx
@@ -1,5 +1,7 @@
-import { getScopedI18n } from "@/locales/server";
+import { getI18n, getScopedI18n } from "@/locales/server";
 import Text from "../Text/Text";
+import Tooltip from "../Tooltip/Tooltip";
+import Link from "next/link";
 
 const statuses = [
   { name: "available", color: "bg-primary" },
@@ -8,7 +10,8 @@ const statuses = [
 ];
 
 export default async function Status() {
-  const t = await getScopedI18n("status");
+  const scoped = await getScopedI18n("status");
+  const t = await getI18n();
 
   let { name, color } = statuses[0];
 
@@ -18,16 +21,21 @@ export default async function Status() {
     | "veryBusy";
 
   return (
-    <li className="center gap-2 text-dark bg-light px-2 py-1 rounded-full">
-      <div className="relative">
-        <div
-          className={`${color} w-4 aspect-square rounded-full animate-ping`}
-        />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className={`${color} w-3 aspect-square rounded-full`} />
+    <Tooltip content={t("backToHome")} variant="dark">
+      <Link
+        href="/"
+        className="center gap-2 text-dark bg-light px-2 py-1 rounded-full"
+      >
+        <div className="relative">
+          <div
+            className={`${color} w-4 aspect-square rounded-full animate-ping`}
+          />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <div className={`${color} w-3 aspect-square rounded-full`} />
+          </div>
         </div>
-      </div>
-      <Text type="expanded">{t(key)}</Text>
-    </li>
+        <Text type="expanded">{scoped(key)}</Text>
+      </Link>
+    </Tooltip>
   );
 }
