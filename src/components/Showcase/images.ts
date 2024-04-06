91785d223274c3c18a31dee8b1484373cb5ec852
Pasztor Isti <isticsek@gmail.com>
2024-04-07 01:17:06 +0300

    create images array with test images for now

---
+++ src/components/Showcase/images.ts
@@ -0,0 +1,50 @@
+export const imageList = [
+  {
+    title: "Salon Manager",
+    src: "/images/showcase/01.webp",
+    width: 400,
+    height: 400,
+  },
+  {
+    title: "Salon Manager",
+    src: "/images/showcase/02.webp",
+    width: 400,
+    height: 400,
+  },
+  {
+    title: "Salon Manager",
+    src: "/images/showcase/03.webp",
+    width: 400,
+    height: 400,
+  },
+  {
+    title: "Salon Manager",
+    src: "/images/showcase/04.webp",
+    width: 400,
+    height: 400,
+  },
+  {
+    title: "Salon Manager",
+    src: "/images/showcase/05.webp",
+    width: 400,
+    height: 400,
+  },
+  {
+    title: "Salon Manager",
+    src: "/images/showcase/06.webp",
+    width: 400,
+    height: 400,
+  },
+  {
+    title: "Salon Manager",
+    src: "/images/showcase/07.webp",
+    width: 400,
+    height: 400,
+  },
+  {
+    title: "Salon Manager",
+    src: "/images/showcase/08.webp",
+    width: 700,
+    height: 400,
+  },
+];
