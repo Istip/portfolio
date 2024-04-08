93056a18caa7b0ef033be5018357a1a27d485c00
Pasztor Isti <isticsek@gmail.com>
2024-04-08 16:36:38 +0300

    create test about page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,3 +1,14 @@
+import PageContainer from "@/components/Container/Container";
+
 export default function AboutPage() {
-  return <div>About</div>;
+  return (
+    <PageContainer>
+      <div className="text-7xl">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, cum
+        dolor quod dolorum quibusdam ratione odit dolores. Recusandae minima
+        ipsum inventore dolore sint iste? Ipsa, officia! Adipisci distinctio
+        quidem unde?
+      </div>
+    </PageContainer>
+  );
 }
