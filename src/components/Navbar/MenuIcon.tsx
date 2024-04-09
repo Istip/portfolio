ff60d25d6a2fbedc031021905760332f443b7691
Pasztor Isti <isticsek@gmail.com>
2024-04-09 18:22:46 +0300

    fix finally the navbar issues

--- src/components/Navbar/MenuIcon.tsx
+++ src/components/Navbar/MenuIcon.tsx
@@ -1,29 +1,35 @@
-"use client";
-
-import { useState } from "react";
 import { useI18n } from "@/locales/client";
-import { AnimatePresence } from "framer-motion";
 import Text from "../Text/Text";
-import Menu from "./Menu";
 
-export default function MenuIcon() {
-  const [open, setOpen] = useState(false);
+interface Props {
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  text: "text-dark" | "text-light";
+}
+
+export default function MenuIcon({ setOpen, text }: Props) {
   const t = useI18n();
+
+  const bg = text === "text-dark" ? "bg-dark" : "bg-light";
 
   return (
     <>
-      <AnimatePresence>{open && <Menu setOpen={setOpen} />}</AnimatePresence>
       <li>
         <button
           className="center gap-2 group uppercase"
           onClick={() => setOpen(true)}
         >
           <Text>{t("menu")}</Text>
-          <div className="transition-all">
-            <span className="sr-only">Menu</span>
-            <div className="h-[1px] w-8 transition-all duration-200 group-hover:w-16 bg-light rounded-full mb-1" />
-            <div className="h-[1px] w-8 transition-all duration-200 group-hover:w-16 bg-light rounded-full mb-1" />
-            <div className="h-[1px] w-8 transition-all duration-200 group-hover:w-16 bg-light rounded-full" />
+          <div className={`transition-all`}>
+            <span className="sr-only">{t("menu")}</span>
+            <div
+              className={`h-[1px] w-8 transition-all duration-200 group-hover:w-16 ${bg} ${text} rounded-full mb-1`}
+            />
+            <div
+              className={`h-[1px] w-8 transition-all duration-200 group-hover:w-16 ${bg} ${text} rounded-full mb-1`}
+            />
+            <div
+              className={`h-[1px] w-8 transition-all duration-200 group-hover:w-16 ${bg} ${text} rounded-full`}
+            />
           </div>
         </button>
       </li>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,34 @@
+"use client";
+
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+import { AnimatePresence } from "framer-motion";
 import LanguageSelector from "./LanguageSelector";
 import Status from "./Status";
 import MenuIcon from "./MenuIcon";
+import Menu from "./Menu";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const pathname = usePathname();
+
+  const bg = pathname !== "/" ? "bg-transparent" : "bg-dark";
+  const text = pathname !== "/" ? "text-dark" : "text-light";
+
   return (
-    <nav className="w-full fixed z-10 p-5 text-light">
-      <ul className="flex items-center justify-between mx-auto">
-        <Status />
-        <ul className="center gap-4 px-2 pb-2 bg-dark rounded-bl-2xl">
-          <LanguageSelector />
-          <MenuIcon />
+    <>
+      <AnimatePresence>{open && <Menu setOpen={setOpen} />}</AnimatePresence>
+      <nav className={`w-full fixed z-10 p-5 ${text}`}>
+        <ul className="flex items-center justify-between mx-auto">
+          <Status />
+          <ul className={`center gap-4 px-2 pb-2 rounded-bl-2xl ${bg}`}>
+            <LanguageSelector />
+            <MenuIcon setOpen={setOpen} text={text} />
+          </ul>
         </ul>
-      </ul>
-    </nav>
+      </nav>
+    </>
   );
 };
 
