05db52a4d232220176e57b000604fdf619b1efb6
Pasztor Isti <isticsek@gmail.com>
2024-04-09 20:36:41 +0300

    add animation for showcase button

--- src/components/Showcase/Showcase.tsx
+++ src/components/Showcase/Showcase.tsx
@@ -35,7 +35,7 @@ export default function Showcase() {
             >
               <div className="absolute top-0 bottom-0 left-0 right-0 bg-dark/50 mr-5 transition-opacity opacity-0 hover:opacity-100 center duration-500">
                 <Text as="span" type="expandedBold">
-                  <div className="px-4 py-2 bg-light text-dark rounded-full">
+                  <div className="px-4 py-2 bg-light border border-dark text-dark rounded-full transition-all hover:text-light hover:bg-dark">
                     {title}
                   </div>
                 </Text>
@@ -69,7 +69,7 @@ export default function Showcase() {
             >
               <div className="absolute top-0 bottom-0 left-0 right-0 bg-dark/50 mr-5 transition-opacity opacity-0 hover:opacity-100 center duration-500">
                 <Text as="span" type="expandedBold">
-                  <div className="px-4 py-2 bg-light text-dark rounded-full">
+                  <div className="px-4 py-2 bg-light border border-dark text-dark rounded-full transition-all hover:text-light hover:bg-dark">
                     {title}
                   </div>
                 </Text>
