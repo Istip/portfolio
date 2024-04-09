6aab31222c119fd1dc3764abc20ee4daeaafc4c5
Pasztor Isti <isticsek@gmail.com>
2024-04-09 19:31:31 +0300

    create 3 more pages

---
+++ src/app/[locale]/services/page.tsx
@@ -0,0 +1,15 @@
+import PageContainer from "@/components/Container/Container";
+
+export default function ServicesPage() {
+  return (
+    <PageContainer>
+      ServicesPage
+      <div className="text-7xl">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, cum
+        dolor quod dolorum quibusdam ratione odit dolores. Recusandae minima
+        ipsum inventore dolore sint iste? Ipsa, officia! Adipisci distinctio
+        quidem unde?
+      </div>
+    </PageContainer>
+  );
+}
