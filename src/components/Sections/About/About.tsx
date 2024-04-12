71381aebd1fe759b4356f179a9f716031c0c3bdc
Pasztor Isti <isticsek@gmail.com>
2024-04-12 21:07:58 +0300

    remove empty lines

--- src/components/Sections/About/About.tsx
+++ src/components/Sections/About/About.tsx
@@ -22,7 +22,6 @@ export default async function About() {
         </div>
         <div className="w-full aspect-square bg-primaryDark hover:bg-dark hover:text-light transition-colors duration-500 rounded-2xl center gap-4 flex-col">
           <Icon name="webapp" size={64} />
-
           <Text className="text-md tracking-wide" type="expandedBold">
             {t("webapps")}
           </Text>
@@ -36,14 +35,12 @@ export default async function About() {
       <div className="grid grid-cols-2 gap-10">
         <div className="w-full aspect-square bg-primaryDark hover:bg-dark hover:text-light transition-colors duration-500 rounded-2xl center gap-4 flex-col">
           <Icon name="ui" size={64} />
-
           <Text className="text-md tracking-wide" type="expandedBold">
             {t("ui")}
           </Text>
         </div>
         <div className="w-full aspect-square bg-primaryDark hover:bg-dark hover:text-light transition-colors duration-500 rounded-2xl center gap-4 flex-col">
           <Icon name="ux" size={64} />
-
           <Text className="text-md tracking-wide" type="expandedBold">
             {t("ux")}
           </Text>
