07d5c4fd17576c70482db1d4dd6316d17816ad4d
Pasztor Isti <isticsek@gmail.com>
2024-04-14 19:48:16 +0300

    create tilt card compo

---
+++ src/components/Sections/About/TiltCard.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Tilt } from "react-tilt";
+
+export default function TiltCard({ children }: { children: React.ReactNode }) {
+  const options = {
+    reverse: false,
+    max: 35,
+    perspective: 1000,
+    scale: "1",
+    speed: 1000,
+    transition: true,
+    axis: null,
+    reset: true,
+  };
+
+  return (
+    <Tilt options={options} className="relative group">
+      <div className="relative z-10">{children}</div>
+      <div className="absolute w-full h-full bg-primaryDark transition-all top-0 left-0 scale-100 rounded-3xl group-hover:rounded-3xl group-hover:scale-105" />
+    </Tilt>
+  );
+}
