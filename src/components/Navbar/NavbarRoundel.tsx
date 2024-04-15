d2febcdeee00019619141cd148bfa78a280864da
Pasztor Isti <isticsek@gmail.com>
2024-04-15 20:17:29 +0300

    create rouded corner svg component for navbar

---
+++ src/components/Navbar/NavbarRoundel.tsx
@@ -0,0 +1,20 @@
+export default function NavbarRoundel() {
+  return (
+    <>
+      <svg
+        width="8"
+        height="8"
+        viewBox="0 0 8 8"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M8 0H0C4.41846 0 8 3.58167 8 8V0Z"
+          fill="currentColor"
+        />
+      </svg>
+    </>
+  );
+}
