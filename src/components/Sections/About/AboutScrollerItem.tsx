558d033a0563ba9f1162374292ef4f1be2b23c98
Pasztor Isti <isticsek@gmail.com>
2024-04-20 00:02:39 +0300

    improve animations for scroller item

--- src/components/Sections/About/AboutScrollerItem.tsx
+++ src/components/Sections/About/AboutScrollerItem.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import Text from "@/components/Text/Text";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
 
 interface Props {
   title: string;
@@ -6,8 +10,16 @@ interface Props {
 }
 
 export default function AboutScrollerItem({ title, descripton }: Props) {
+  const ref = useRef<HTMLDivElement>(null);
+  const isInView = useInView(ref, { margin: "0px -70% 0px -20%" });
+
   return (
-    <div className="w-[500px] h-full flex items-center flex-col gap-4 text-center p-10">
+    <div
+      ref={ref}
+      className={`${
+        isInView ? "opacity-100 scale-100" : "opacity-10 scale-50"
+      } transition-all duration-500 w-[500px] h-full flex items-center flex-col gap-4 text-center p-10`}
+    >
       <Text
         as="h3"
         type="expandedBold"
