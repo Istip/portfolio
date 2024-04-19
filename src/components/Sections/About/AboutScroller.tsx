4b324ee7933de9e21663f6717efea4f283118140
Pasztor Isti <isticsek@gmail.com>
2024-04-20 00:42:39 +0300

    improve more the about scroller compo

--- src/components/Sections/About/AboutScroller.tsx
+++ src/components/Sections/About/AboutScroller.tsx
@@ -2,28 +2,51 @@
 
 import { useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
-import { useScopedI18n } from "@/locales/client";
+import { useI18n, useScopedI18n } from "@/locales/client";
 import AboutScrollerItem from "./AboutScrollerItem";
 import Icon from "@/components/Icon/Icon";
+import Text from "@/components/Text/Text";
 
 const AboutScroller = () => {
   const targetRef = useRef(null);
   const { scrollYProgress } = useScroll({ target: targetRef });
   const x = useTransform(scrollYProgress, [0, 1], ["40%", "-80%"]);
 
-  const t = useScopedI18n("hobby");
+  const scopedT = useScopedI18n("hobby");
+  const t = useI18n();
 
   const sections = [
-    { title: t("improve.title"), descripton: t("improve.description") },
-    { title: t("design.title"), descripton: t("design.description") },
-    { title: t("pet.title"), descripton: t("pet.description") },
-    { title: t("games.title"), descripton: t("games.description") },
-    { title: t("football.title"), descripton: t("football.description") },
-    { title: t("bikes.title"), descripton: t("bikes.description") },
+    {
+      title: scopedT("improve.title"),
+      descripton: scopedT("improve.description"),
+    },
+    {
+      title: scopedT("design.title"),
+      descripton: scopedT("design.description"),
+    },
+    {
+      title: scopedT("pet.title"),
+      descripton: scopedT("pet.description"),
+    },
+    {
+      title: scopedT("games.title"),
+      descripton: scopedT("games.description"),
+    },
+    {
+      title: scopedT("football.title"),
+      descripton: scopedT("football.description"),
+    },
+    {
+      title: scopedT("bikes.title"),
+      descripton: scopedT("bikes.description"),
+    },
   ];
 
   return (
     <section ref={targetRef} className="relative h-[300vh] rounded-2xl">
+      <Text type="expandedBold" className="text-3xl md:p-[50px] p-5">
+        {t("freeTime")}
+      </Text>
       <div className="sticky top-0 flex h-screen items-center overflow-hidden">
         <div className="absolute top-1/2 left-0 bottom-1/2 my-auto">
           <div className="p-0.5 center ml-2 rounded-full bg-light text-dark opacity-50">
