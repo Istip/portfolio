02ebd5e078c05503454e1f477c39c317f77306ad
Pasztor Isti <isticsek@gmail.com>
2024-04-28 17:12:16 +0300

    update import order

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,7 +1,7 @@
 import PageContainer from "@/components/Container/Container";
-import Footer from "@/components/Footer/Footer";
 import AboutScrollerSection from "@/components/Sections/About/AboutScrollerSection/AboutScrollerSection";
 import AboutSection from "@/components/Sections/About/AboutSection/AboutSection";
+import Footer from "@/components/Footer/Footer";
 
 export default function AboutPage() {
   return (
