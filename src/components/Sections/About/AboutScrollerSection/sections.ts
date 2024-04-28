9cc56f36b4fe8d67240f9150d80c25f9d6594d7a
Pasztor Isti <isticsek@gmail.com>
2024-04-28 16:20:03 +0300

    create separate exportable sections data file

---
+++ src/components/Sections/About/AboutScrollerSection/sections.ts
@@ -0,0 +1,26 @@
+export const sections = [
+  {
+    title: "improve.title",
+    descripton: "improve.description",
+  },
+  {
+    title: "design.title",
+    descripton: "design.description",
+  },
+  {
+    title: "pet.title",
+    descripton: "pet.description",
+  },
+  {
+    title: "games.title",
+    descripton: "games.description",
+  },
+  {
+    title: "football.title",
+    descripton: "football.description",
+  },
+  {
+    title: "bikes.title",
+    descripton: "bikes.description",
+  },
+];
