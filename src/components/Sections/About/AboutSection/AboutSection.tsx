926d5b66a5c7bef61a448c756653f74f6c5d36fe
Pasztor Isti <isticsek@gmail.com>
2024-04-28 17:11:35 +0300

    add prop to hide title

--- src/components/Sections/About/AboutSection/AboutSection.tsx
+++ src/components/Sections/About/AboutSection/AboutSection.tsx
@@ -1,9 +1,13 @@
 import Section from "../../Section/Section";
 import About from "./About";
 
-export default async function AboutSection() {
+export default async function AboutSection({
+  showTitle = true,
+}: {
+  showTitle?: boolean;
+}) {
   return (
-    <Section number={1} title="about">
+    <Section number={1} title="about" showTitle={showTitle}>
       <About />
     </Section>
   );
