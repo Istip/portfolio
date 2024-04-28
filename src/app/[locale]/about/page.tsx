21d7334b4ff525edb1eee39456903bb3a3006573
Pasztor Isti <isticsek@gmail.com>
2024-04-28 17:33:29 +0300

    create placeholder compo for more about me

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -2,12 +2,14 @@ import PageContainer from "@/components/Container/Container";
 import AboutScrollerSection from "@/components/Sections/About/AboutScrollerSection/AboutScrollerSection";
 import AboutSection from "@/components/Sections/About/AboutSection/AboutSection";
 import Footer from "@/components/Footer/Footer";
+import AboutMore from "@/components/Sections/About/AboutScrollerSection/AboutMore";
 
 export default function AboutPage() {
   return (
     <>
       <PageContainer>
         <AboutSection showTitle={false} />
+        <AboutMore />
         <AboutScrollerSection />
       </PageContainer>
       <Footer />
---
+++ src/components/Sections/About/AboutScrollerSection/AboutMore.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AboutMore() {
+  return <div className="bg-primary">AboutMore</div>;
+}
