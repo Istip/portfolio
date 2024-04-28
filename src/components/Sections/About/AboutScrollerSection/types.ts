832632d5313859f50f8fc9e1eb3114cc5a1c124e
Pasztor Isti <isticsek@gmail.com>
2024-04-28 16:22:47 +0300

    add title and description types

---
+++ src/components/Sections/About/AboutScrollerSection/types.ts
@@ -0,0 +1,15 @@
+export type Title =
+  | "improve.title"
+  | "design.title"
+  | "pet.title"
+  | "games.title"
+  | "bikes.title"
+  | "football.title";
+
+export type Description =
+  | "improve.description"
+  | "design.description"
+  | "pet.description"
+  | "games.description"
+  | "bikes.description"
+  | "football.description";
