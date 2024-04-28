e934b8372fd91f2daf3c0b9f6eab09bf39fc2f8f
Pasztor Isti <isticsek@gmail.com>
2024-04-28 16:20:17 +0300

    remove translated sections

--- src/components/Sections/About/AboutScrollerSection/AboutScrollerSection.tsx
+++ src/components/Sections/About/AboutScrollerSection/AboutScrollerSection.tsx
@@ -1,44 +1,15 @@
 "use client";
 
-import { useScopedI18n } from "@/locales/client";
 import Section from "../../Section/Section";
 import AboutScroller from "./AboutScroller";
 import useWindowSize from "@/hooks/useWindowSize";
 import FreeTime from "./FreeTime";
 import AboutScrollerMarquee from "./AboutScrollerMarquee";
+import { sections } from "./sections";
 
 export default function AboutScrollerSection() {
   const { width } = useWindowSize();
   const largeScreen = width && width >= 1024;
-
-  const scopedT = useScopedI18n("hobby");
-
-  const sections = [
-    {
-      title: scopedT("improve.title"),
-      descripton: scopedT("improve.description"),
-    },
-    {
-      title: scopedT("design.title"),
-      descripton: scopedT("design.description"),
-    },
-    {
-      title: scopedT("pet.title"),
-      descripton: scopedT("pet.description"),
-    },
-    {
-      title: scopedT("games.title"),
-      descripton: scopedT("games.description"),
-    },
-    {
-      title: scopedT("football.title"),
-      descripton: scopedT("football.description"),
-    },
-    {
-      title: scopedT("bikes.title"),
-      descripton: scopedT("bikes.description"),
-    },
-  ];
 
   if (!largeScreen) {
     return (
