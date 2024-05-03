abf3ae37f39d1494f2d768ddfa860bcd7f59c2a5
Pasztor Isti <isticsek@gmail.com>
2024-05-03 16:03:37 +0300

    apply new about cards component

--- src/components/Sections/About/AboutSection/About.tsx
+++ src/components/Sections/About/AboutSection/About.tsx
@@ -1,72 +1,14 @@
 import { getI18n } from "@/locales/server";
 import Text from "../../../Text/Text";
-import Icon from "@/components/Icon/Icon";
-import TiltCard from "./TiltCard";
+import AboutCards from "./AboutCards";
 
 export default async function About() {
   const t = await getI18n();
 
   return (
-    <div>
+    <>
       <Text className="text-xl sm:text-3xl mb-16">{t("aboutIntro")}</Text>
-
-      <Text type="expandedBold" className="text-3xl my-5">
-        {t("iDevelop")}
-      </Text>
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
-        <TiltCard>
-          <div className="w-full aspect-square transition-all bg-primaryDark hover:bg-dark hover:text-light duration-500 rounded-2xl center gap-4 flex-col">
-            <Icon name="website" size={64} />
-            <Text
-              className="text-md tracking-wide text-center"
-              type="expandedBold"
-            >
-              {t("websites")}
-            </Text>
-          </div>
-        </TiltCard>
-        <TiltCard>
-          <div className="w-full aspect-square bg-primaryDark hover:bg-dark hover:text-light transition-all duration-500 rounded-2xl center gap-4 flex-col">
-            <Icon name="webapp" size={64} />
-            <Text
-              className="text-md tracking-wide text-center"
-              type="expandedBold"
-            >
-              {t("webapps")}
-            </Text>
-          </div>
-        </TiltCard>
-      </div>
-
-      <Text type="expandedBold" className="text-3xl mb-5 mt-16">
-        {t("iDesign")}
-      </Text>
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
-        <TiltCard>
-          <div className="w-full aspect-square bg-primaryDark hover:bg-dark hover:text-light transition-all duration-500 rounded-2xl center gap-4 flex-col">
-            <Icon name="ui" size={64} />
-            <Text
-              className="text-md tracking-wide text-center"
-              type="expandedBold"
-            >
-              {t("ui")}
-            </Text>
-          </div>
-        </TiltCard>
-        <TiltCard>
-          <div className="w-full aspect-square bg-primaryDark hover:bg-dark hover:text-light transition-all duration-500 rounded-2xl center gap-4 flex-col">
-            <Icon name="ux" size={64} />
-            <Text
-              className="text-md tracking-wide text-center"
-              type="expandedBold"
-            >
-              {t("ux")}
-            </Text>
-          </div>
-        </TiltCard>
-      </div>
-    </div>
+      <AboutCards />
+    </>
   );
 }
