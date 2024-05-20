5b6ae7345a6f1337f9be1c47a05acc08b22574bf
Pasztor Isti <isticsek@gmail.com>
2024-05-20 20:33:21 +0300

    render new stuff to dash

---
+++ src/app/[locale]/admin/page.tsx
@@ -0,0 +1,10 @@
+import Messages from "@/components/Dashboard/Messages";
+import React from "react";
+
+export default function AdminPage() {
+  return (
+    <>
+      <Messages />
+    </>
+  );
+}
