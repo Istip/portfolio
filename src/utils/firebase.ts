97501a72e91403e4bade8981bc2763f325e3d3a1
Pasztor Isti <isticsek@gmail.com>
2024-05-21 19:27:23 +0300

    add firebase auth

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const config = {
@@ -13,3 +14,4 @@ const config = {
 const app = initializeApp(config);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
